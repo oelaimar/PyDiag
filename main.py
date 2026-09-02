058843230ed4a580f75691e18f5bcd681301ab07
oelaimar <oussama.el.aimar@gmail.com>
2026-09-02 15:11:46 +0100

    correct the module 1 2 and 3

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-def calcul_moyen(notes):
+print("===MODULE I===")
+#module 1
+student_karim = {"name" : "Karim", "prenom" : "Ben Ali", "notes" : [12, 15, 9]}
+
+def calculer_moyenne(notes):
     sum_notes = 0
     count_notes = 0
     for note in notes:
         sum_notes += note
         count_notes += 1
     return sum_notes / count_notes
+
+print(f"{student_karim["name"]} moyenne : {(float(calculer_moyenne(student_karim["notes"]))):.2f}")
+
+print("===MODULE II===")
+#module 2
+
+valeurs_test = [9.9, 10.0, 11.9, 12.0, 15.9, 16.0, 20.0]
 
 def appreciation(moyenne):
     if moyenne < 10:
@@ -15,33 +26,73 @@ def appreciation(moyenne):
         return "Bien"
     elif moyenne >= 16 and moyenne <= 20:
         return "Tres bien"
-     
-students = []
-
-while True:    
-    first_name = input("type first name: ")
-    last_name = input ("type last name: ")
-    notes = []
-    is_valide_numbers = True
-    for i in range(3):
-        user_input = input(f"type num {i + 1} : ")
-        try :
-            note = float(user_input)
-        except ValueError:
-            note = "this is not a valide number"
-            is_valide_numbers = False
-        notes.append(note)
     
-    if not is_valide_numbers:
-        for note in notes:
-            if isinstance(note, float):
-                print(f"note accepted: {note}")
-            else:
-                print(note)
+for value in valeurs_test:
+    print(f"{value} -> {appreciation(value)}")
     
-    else:
-        moyen = calcul_moyen(notes)
-        print(f"{first_name} {last_name} : {moyen:.2f}")
+students = [
+        {"nom": "Karim", "notes": [12, 15, 9]},
+        {"nom": "Sara", "notes": [18, 17, 16]},
+        {"nom": "Lina", "notes": [6, 8, 5]},
+    ]
+    
+def desplay_students(students):
+    bad_student = {}
+    min = 20
+    best_student = {}
+    max = 0
+    for student in students:
+        moyenne =  float(calculer_moyenne(student["notes"]))
+        
+        if moyenne > max:
+            max = moyenne
+            best_student = student
+        if moyenne < min:
+            min = moyenne
+            bad_student = student
+        print(f"{student["nom"]} {moyenne:.2f} {appreciation(moyenne)}")
+        
+        print(f"best student is {best_student["nom"]}")
+        print(f"bad student is {bad_student["nom"]}")
+        
+desplay_students(students)
 
 
-    
+
+print("==MODULE III===")
+
+students = [
+    {"nom": "Karim", "notes": [12, 15, 9]},
+    {"nom": "Sara", "notes": [18, 17, 16]},
+    {"nom": "Lina", "notes": [6, 8, 5]},
+    ]
+
+def construire_resultats(students):
+    result = {}
+    for student in students:
+        moyenne = calculer_moyenne(student["notes"])
+        mention = appreciation(moyenne)
+        result[student["nom"]] = {"moyenne": moyenne, "mention": mention}
+    return result
+
+result = construire_resultats(students)
+
+for value in result:
+    value_dic = {value : result[value]}
+    print(value_dic)
+
+print("==result sorted==")
+
+result_sorted = dict(sorted(result.items() , key = lambda student : student[1]["moyenne"], reverse= True))
+
+for value in result_sorted:
+    value_dic = {value : result[value]}
+    print(value_dic)
+
+students_failed = []
+for student in students:
+    moyenne = calculer_moyenne(student["notes"])
+    if moyenne < 10:
+        students_failed.append((student["nom"], moyenne))
+
+print(students_failed)
