3f6a6c47296c46990d49cb11d9fde283cf545aac
oelaimar <oussama.el.aimar@gmail.com>
2026-09-08 10:03:41 +0100

    chalange poo black 1 et 2

---
+++ challenge_poo_systemes.py
@@ -0,0 +1,104 @@
+class ErrorDisponible(Exception):
+    pass
+
+class Livre:
+    def __init__(self, titre, auteur, disponible = True):
+        self.titre = titre
+        self.auteur = auteur
+        self.disponible = disponible
+
+    def emprunter(self):
+        self.disponible = False
+    def rendre(self):
+        self.disponible = True
+    def __str__(self):
+        return f"\"{self.titre}\" de {self.auteur}  -- : {"disponible" if self.disponible else "emprunte"}"
+
+class Adherent:
+    banque = "Ma Banque"
+    nombre_comptes = 0
+    def __init__(self, nom):
+        self.nom = nom
+        self.livres_empruntes = []
+    def emprunter_livre(self, livre : Livre):
+        try:
+            if not livre.disponible:
+                raise ErrorDisponible
+            livre.emprunter()
+            self.livres_empruntes.append(livre)
+        except ErrorDisponible:
+            print(f"Erreur : le livre \"{livre.titre}\" n'est pas disponible.")
+
+    def rendre_livre(self, livre : Livre):
+        try:
+            if livre not in self.livres_empruntes:
+                raise ErrorDisponible
+            livre.rendre()
+            self.livres_empruntes.remove(livre)
+        except ErrorDisponible:
+            print(f"Erreur : le livre \"{livre.titre}\" n'est pas disponible.")
+    def nombre_livres_empruntes(self):
+        return len(self.livres_empruntes)
+
+livre = Livre("Dune", "Frank Herbert")
+print(livre)
+
+livre = Livre("Dune", "Frank Herbert")
+ali = Adherent("Ali")
+ali.emprunter_livre(livre)
+print(livre)
+print(ali.nombre_livres_empruntes())
+
+livre = Livre("Dune", "Frank Herbert")
+ali = Adherent("Ali")
+sara = Adherent("Sara")
+ali.emprunter_livre(livre)
+sara.emprunter_livre(livre)
+
+livre = Livre("Dune", "Frank Herbert")
+ali = Adherent("Ali")
+ali.emprunter_livre(livre)
+ali.rendre_livre(livre)
+print(livre)
+print(ali.nombre_livres_empruntes())
+
+
+class CompteBancaire:
+
+    nom_banque = "BanquePyDiag"
+    nombre_comptes = 0
+
+    def __init__(self, nom, solde_initial = 0):
+        self.__sold = solde_initial
+        self.nom = nom
+        CompteBancaire.nom_banque += 1
+
+    @property
+    def solde(self):
+        return self.__sold
+
+    def deposer(self, montant):
+        if montant < 0:
+            print("fonds suffisants")
+        self.__sold += montant
+
+    def retirer(self, montant):
+        if montant < 0:
+            print("fonds suffisants")
+        self.__sold -= montant
+
+    @classmethod
+    def nombre_total_comptes(cls):
+        return cls.nombre_comptes
+
+    @staticmethod
+    def convertir_devise(montant, taux):
+        return montant * taux
+
+
+compte = CompteBancaire("Ali", solde_initial=100)
+print(compte.solde)
+try:
+    compte.solde = 5000
+except:
+    print("AttributeError: can't set attribute 'solde'")
